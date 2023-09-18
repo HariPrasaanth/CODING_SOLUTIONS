ae66f8e548ebfab12fba2754b4f3c79acec7274d
unknown <hariprasaanth1998@gmail.com>
2023-09-18 19:15:57 +0530

    Library Fine

---
+++ HackerRank/Problem Solving/Algorithms/libraryFine.js
@@ -0,0 +1,30 @@
+//Library Fine
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    // Write your code here
+    if(y1<y2){
+        return 0
+    }
+    else{
+        if(y1>y2){
+            return 10000
+        }
+        else{
+            if(m1<m2){
+                return 0
+            }
+            else{
+                if(m1>m2){
+                    return (m1-m2) * 500
+                }
+                else{
+                    if(d1>d2){
+                        return (d1-d2) * 15
+                    }
+                    else{
+                        return 0
+                    }
+                }
+            }
+        }
+    }
+}
